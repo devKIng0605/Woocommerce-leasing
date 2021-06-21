133e45120165b29f7ccec68799628108ebea6c0b
devKIng0605 <webdevking0605@gmail.com>
2021-06-21 16:47:41 +0300

    update offers overview template

--- themes/default/offers-overview-template.php
+++ themes/default/offers-overview-template.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: FindLeasing Car Detail
  * Description: A Page Template with a darker design.
@@ -46,81 +47,80 @@ $action_url = home_url($wp->request);
 
 ?>
 
-    <div id="main-content" class="main-content">
+<div id="main-content" class="main-content fl-listing-page">
 
-        <div id="primary" class="content-area">
-            <div id="content" class="site-main" role="main">
+    <div id="primary" class="content-area">
+        <div id="content" class="site-main" role="main">
 
-                <?php get_template_part('template-parts/content', 'page'); ?>
+            <?php get_template_part('template-parts/content', 'page'); ?>
 
-                <div class="fl-bs">
-                    <div class="row">
-                        <div class="col-6">
-                            <?php if (sizeof($makes) < 5) { ?>
-                                <ul class="fl-sorting-container">
-                                    <?php
-                                    foreach ($makes as $make) { ?>
-                                        <li>
-                                            <a href="<?php echo fl_get_params('make', strtolower($make['name'])); ?>"><?php echo $make['name']; ?></a>
-                                        </li>
-                                    <?php } ?>
-                                </ul>
-                            <?php } else { ?>
-                                <form method="get" action="<?php echo esc_attr($action_url); ?>">
-                                    <select name="make" onchange="this.form.submit()">
-                                        <option value="" <?php $make_filter == '' ? 'selected' : '' ?>>Vælg mærke
-                                        </option>
-                                        <?php foreach ($makes as $make) {
-                                            $_make = strtolower($make['name']);
-                                            ?>
-                                            <option value="<?php echo $_make; ?>" <?php echo($_make == strtolower($make_filter) ? 'selected' : '') ?>><?php echo $make['name']; ?></option>
-                                        <?php } ?>
-                                    </select>
-                                </form>
-                            <?php } ?>
-                        </div>
-                        <div class="col-6 text-right">
+            <div class="fl-bs">
+                <div class="row">
+                    <div class="col-6">
+                        <?php if (sizeof($makes) < 5) { ?>
                             <ul class="fl-sorting-container">
-                                <li class="hidden-xs text-muted">Sortering:</li>
-                                <li>
-                                    <a href="<?php echo fl_get_params('ordering', ($_order == 'make' && !$desc ? '-' : '') . 'make'); ?>">Mærke <?php echo $_order == 'make' ? '<i class="fa fa-caret-' . ($desc ? 'up' : 'down') . '"></i>' : ''; ?></a>
-                                </li>
-                                <li>
-                                    <a href="<?php echo fl_get_params('ordering', ($_order == 'year' && !$desc ? '-' : '') . 'year'); ?>">Årgang <?php echo $_order == 'year' ? '<i class="fa fa-caret-' . ($desc ? 'down' : 'up') . '"></i>' : ''; ?></a>
-                                </li>
-                                <li>
-                                    <a href="<?php echo fl_get_params('ordering', ($_order == 'price_monthly' && !$desc ? '-' : '') . 'price_monthly'); ?>">Pris <?php echo $_order == 'price_monthly' ? '<i class="fa fa-caret-' . ($desc ? 'up' : 'down') . '"></i>' : ''; ?></a>
-                                </li>
-                                <li>
-                                    <a href="<?php echo fl_get_params('ordering', ($_order == 'id' && !$desc ? '-' : '') . 'id'); ?>">Nyeste <?php echo $_order == 'id' ? '<i class="fa fa-caret-' . ($desc ? 'up' : 'down') . '"></i>' : ''; ?></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <?php
-                        foreach ($response['results'] as $offer) {
-                            echo apply_filters('findleasing-leasing-preview', $offer);
-                        }
-                        ?>
-                    </div>
-                    <div class="row">
-                        <div class="col-12" style="text-align: center;">
-                            <ul class="fl-pagination">
                                 <?php
-                                for ($i = 1; $i < $num_pages + 1; $i++): ?>
+                                foreach ($makes as $make) { ?>
                                     <li>
-                                        <a href="?page_num=<?php echo $i; ?>&make=<?php echo $make_filter; ?>&ordering=<?php echo $ordering; ?>"
-                                           class="<?php echo($i === $page ? 'active' : '') ?>"><?php echo $i; ?></a>
+                                        <a href="<?php echo fl_get_params('make', strtolower($make['name'])); ?>"><?php echo $make['name']; ?></a>
                                     </li>
-                                <?php endfor; ?>
+                                <?php } ?>
                             </ul>
-                        </div>
+                        <?php } else { ?>
+                            <form method="get" action="<?php echo esc_attr($action_url); ?>">
+                                <select name="make" onchange="this.form.submit()">
+                                    <option value="" <?php $make_filter == '' ? 'selected' : '' ?>>Vælg mærke
+                                    </option>
+                                    <?php foreach ($makes as $make) {
+                                        $_make = strtolower($make['name']);
+                                    ?>
+                                        <option value="<?php echo $_make; ?>" <?php echo ($_make == strtolower($make_filter) ? 'selected' : '') ?>><?php echo $make['name']; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </form>
+                        <?php } ?>
                     </div>
-                </div> <!-- .fl-bs -->
-            </div><!-- #content -->
-        </div><!-- #primary -->
-    </div><!-- #main-content -->
+                    <div class="col-6 text-right">
+                        <ul class="fl-sorting-container">
+                            <li class="hidden-xs text-muted">Sortering:</li>
+                            <li>
+                                <a href="<?php echo fl_get_params('ordering', ($_order == 'make' && !$desc ? '-' : '') . 'make'); ?>">Mærke <?php echo $_order == 'make' ? '<i class="fa fa-caret-' . ($desc ? 'up' : 'down') . '"></i>' : ''; ?></a>
+                            </li>
+                            <li>
+                                <a href="<?php echo fl_get_params('ordering', ($_order == 'year' && !$desc ? '-' : '') . 'year'); ?>">Årgang <?php echo $_order == 'year' ? '<i class="fa fa-caret-' . ($desc ? 'down' : 'up') . '"></i>' : ''; ?></a>
+                            </li>
+                            <li>
+                                <a href="<?php echo fl_get_params('ordering', ($_order == 'price_monthly' && !$desc ? '-' : '') . 'price_monthly'); ?>">Pris <?php echo $_order == 'price_monthly' ? '<i class="fa fa-caret-' . ($desc ? 'up' : 'down') . '"></i>' : ''; ?></a>
+                            </li>
+                            <li>
+                                <a href="<?php echo fl_get_params('ordering', ($_order == 'id' && !$desc ? '-' : '') . 'id'); ?>">Nyeste <?php echo $_order == 'id' ? '<i class="fa fa-caret-' . ($desc ? 'up' : 'down') . '"></i>' : ''; ?></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <?php
+                    foreach ($response['results'] as $offer) {
+                        echo apply_filters('findleasing-leasing-preview', $offer);
+                    }
+                    ?>
+                </div>
+                <div class="row">
+                    <div class="col-12" style="text-align: center;">
+                        <ul class="fl-pagination">
+                            <?php
+                            for ($i = 1; $i < $num_pages + 1; $i++) : ?>
+                                <li>
+                                    <a href="?page_num=<?php echo $i; ?>&make=<?php echo $make_filter; ?>&ordering=<?php echo $ordering; ?>" class="<?php echo ($i === $page ? 'active' : '') ?>"><?php echo $i; ?></a>
+                                </li>
+                            <?php endfor; ?>
+                        </ul>
+                    </div>
+                </div>
+            </div> <!-- .fl-bs -->
+        </div><!-- #content -->
+    </div><!-- #primary -->
+</div><!-- #main-content -->
 
 <?php
 
